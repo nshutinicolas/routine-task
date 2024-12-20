4541d25d0a4671680d7d3bc3e4171988d9505cd7
nshutinicolas <nshuti.nicolas@yahoo.com>
2024-12-20 17:02:13 +0200

    Updated Task detail view | Added more assets

--- Routine Task/TaskDetails/TaskDetails.swift
+++ Routine Task/TaskDetails/TaskDetails.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct TaskItem: Identifiable {
+struct TaskItem: Identifiable, Equatable {
 	let id: String = UUID().uuidString
 	var images: [String]? = nil
 	let title: String
@@ -18,12 +18,13 @@ struct TaskItem: Identifiable {
 struct TaskDetails: View {
 	@Environment(\.dismiss) var dismiss
 	@State private var showAddToListSheet: Bool = false
+	@State private var selectedTasks = [TaskItem]()
 	
 	private let title: String
 	
 	private let mockTaskItems: [TaskItem] = [
 		.init(images: ["hydration", "drinks"], title: "Drink a Glass of Water", description: "Start with a full glass to rehydrate after sleep", benefits: ["Hydration", "Health"]),
-		.init(title: "Deep Breathing Exercise", description: "Practice deep breathinh for 5 minutes"),
+		.init(title: "Deep Breathing Exercise", description: "Practice deep breathinh for 5 minutes", benefits: ["Breathing", "mindfulness", "Calm"]),
 		.init(title: "10-15 minutes of light exercise to energize", description: ""),
 		.init(title: "Short Walk or Jog", description: "Go outside for fresh air and movement", benefits: ["Outdoor", "Movement"])
 	]
@@ -38,21 +39,24 @@ struct TaskDetails: View {
 			header
 				.padding(.horizontal)
 			
-			List{
-				VStack {
-					Text("Wake-Up and Hydration")
-						.font(.cardoRegular(size: 28))
-						.frame(maxWidth: .infinity, alignment: .leading)
-						.listRowSeparator(.hidden)
-					Divider()
+			ScrollView {
+				VStack(spacing: 12) {
+					VStack(spacing: 12) {
+						Text("Wake-Up and Hydration")
+							.font(.cardoRegular(size: 28))
+							.frame(maxWidth: .infinity, alignment: .leading)
+							.listRowSeparator(.hidden)
+						Divider()
+					}
+					LazyVStack(spacing: 12) {
+						ForEach(mockTaskItems) { task in
+							TaskCardView(task: task)
+						}
+					}
 				}
-				ForEach(mockTaskItems) { task in
-					TaskItemView(task: task)
-						.listRowSeparator(.hidden)
-				}
+				.padding(.horizontal)
 			}
 			.scrollIndicators(.hidden)
-			.listStyle(.plain)
 		}
 		.safeAreaInset(edge: .bottom) {
 			HStack {
@@ -73,92 +77,18 @@ struct TaskDetails: View {
 				.background(.buttonBlue)
 				.clipShape(.capsule)
 			}
-			.padding(.horizontal)
+			.padding(.trailing, 24)
 		}
 		.sheet(isPresented: $showAddToListSheet) {
-			VStack {
-				HStack {
-					Text("MY LISTS")
-						.frame(maxWidth: .infinity, alignment: .leading)
-					Image(systemName: "xmark")
-						.font(.caption)
-						.foregroundStyle(.secondary)
-						.padding(8)
-						.background(.tertiary.opacity(0.5))
-						.clipShape(.circle)
-						.onTapGesture {
-							showAddToListSheet = false
-						}
-				}
-				.padding(.top, 16)
-				ScrollView {
-					HStack {
-						Image(systemName: "plus")
-							.font(.body)
-							.frame(width: 58, height: 55)
-							.cornerRadius(6)
-							.background(.buttonBlue.opacity(0.05))
-						Text("ADD TO NEW LIST")
-							.font(.karlRegular(size: 14))
-					}
-					.frame(maxWidth: .infinity, alignment: .leading)
-					HStack {
-						Image(systemName: "star.fill")
-							.font(.body)
-							.foregroundStyle(.buttonBlue)
-							.frame(width: 58, height: 55)
-							.cornerRadius(6)
-							.background(.buttonBlue.opacity(0.05))
-						VStack(alignment: .leading) {
-							Text("ADD TO FAVORITES")
-								.font(.karlRegular(size: 14))
-							Text("23 Items")
-								.font(.footnote)
-						}
-					}
-					.frame(maxWidth: .infinity, alignment: .leading)
-					HStack {
-						Image(systemName: "gear")
-							.font(.body)
-							.foregroundStyle(.buttonBlue)
-							.frame(width: 58, height: 55)
-							.cornerRadius(6)
-							.background(.buttonBlue.opacity(0.05))
-						VStack(alignment: .leading) {
-							Text("MORNING TASKS")
-								.font(.karlRegular(size: 14))
-							Text("10 Items")
-								.font(.footnote)
-						}
-					}
-					.frame(maxWidth: .infinity, alignment: .leading)
-					HStack {
-						Image(systemName: "pencil")
-							.font(.body)
-							.foregroundStyle(.buttonBlue)
-							.frame(width: 58, height: 55)
-							.cornerRadius(6)
-							.background(.buttonBlue.opacity(0.05))
-						VStack(alignment: .leading) {
-							Text("DAILY TASKS")
-								.font(.karlRegular(size: 14))
-							Text("1 Item")
-								.font(.footnote)
-						}
-					}
-					.frame(maxWidth: .infinity, alignment: .leading)
-				}
-				.scrollIndicators(.hidden)
-			}
-			.padding(.horizontal, 8)
-			.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-			.presentationDetents([.medium])
+			BottomSheetContent()
+				.presentationDetents([.medium])
 		}
     }
 	
+	// MARK: Header
 	var header: some View {
-		HStack(spacing: 4) {
-			Image(systemName: "chevron.left")
+		HStack(spacing: 12) {
+			Image(.backIcon)
 				.onTapGesture {
 					dismiss()
 				}
@@ -167,17 +97,22 @@ struct TaskDetails: View {
 				.font(.karlRegular(size: 14))
 				.frame(maxWidth: .infinity, alignment: .leading)
 			Button {
-				// Action
+				selectedTasks = selectedTasks.count == .zero ? mockTaskItems : []
 			} label: {
-				// TODO: Handle the logic
-				Text("Select All")
-					.fontWeight(.semibold)
-					.padding(.horizontal, 12)
-					.padding(.vertical, 8)
-					.background(.skyBlue)
-					.clipShape(.capsule)
+				if selectedTasks.count > .zero {
+					Text("Cancel")
+						.fontWeight(.semibold)
+						.foregroundStyle(.gray)
+				} else {
+					Text("Select All")
+						.fontWeight(.semibold)
+						.foregroundStyle(.white)
+				}
 			}
-			.foregroundStyle(.white)
+			.padding(.horizontal, 13)
+			.padding(.vertical, 7)
+			.background(selectedTasks.count > .zero ? .gray.opacity(0.4) : .skyBlue)
+			.clipShape(.capsule)
 		}
 		.frame(minHeight: 56)
 		.overlay(alignment: .bottom) {
@@ -185,12 +120,14 @@ struct TaskDetails: View {
 		}
 	}
 	
+	// MARK: Item Card
 	@ViewBuilder
-	private func TaskItemView(task: TaskItem) -> some View {
-		VStack {
+	private func TaskCardView(task: TaskItem) -> some View {
+		let isSelected = selectedTasks.contains(where: { $0 == task})
+		VStack(spacing: 16) {
 			if let images = task.images {
 				Rectangle()
-					.frame(height: 240)
+					.frame(height: 237)
 					.overlay {
 						Image(images.first ?? "")
 							.resizable()
@@ -220,7 +157,6 @@ struct TaskDetails: View {
 							.font(.system(size: 14))
 							.italic()
 							.frame(maxWidth: .infinity, alignment: .leading)
-//							.background(.red)
 					}
 					if let benefits = task.benefits {
 						Text(benefits.joined(separator: ", "))
@@ -229,22 +165,115 @@ struct TaskDetails: View {
 					}
 				}
 				.frame(maxWidth: .infinity, alignment: .leading)
-				// TODO: Fix the icon fonts - use custom icon
-				Image(systemName: "plus")
-					.font(.title3)
-					.fontWeight(.medium)
+				Circle()
+					.fill(isSelected ? .skyBlue : Color.skyBlue.opacity(0.15))
 					.frame(width: 38, height: 38)
-					.padding(2)
-					.background(Color.skyBlue.opacity(0.15))
-					.clipShape(.circle)
+					.overlay {
+						if selectedTasks.contains(where: { $0 == task}) {
+							Image(.check)
+								.resizable()
+								.padding(12)
+						} else {
+							Image(.plus)
+								.resizable()
+								.frame(width: 16, height: 16)
+						}
+					}
+					.onTapGesture {
+						if isSelected {
+							selectedTasks = selectedTasks.filter({ $0 != task })
+						} else {
+							selectedTasks.append(task)
+						}
+					}
 			}
-			.padding()
+			.padding(16)
 		}
 		.padding(.bottom, 4)
 		.background(.white)
 		.cornerRadius(20)
-		// TODO: Fix the card Shadow
-		.shadow(color: .yellow, radius: 4)
+		.shadow(color: .skyBlue.opacity(0.1), radius: 6)
+	}
+	
+	// MARK: Bottom sheet content
+	@ViewBuilder
+	private func BottomSheetContent() -> some View {
+		VStack {
+			HStack {
+				Text("MY LISTS")
+					.frame(maxWidth: .infinity, alignment: .leading)
+				Image(systemName: "xmark")
+					.font(.caption)
+					.foregroundStyle(.secondary)
+					.padding(8)
+					.background(.tertiary.opacity(0.5))
+					.clipShape(.circle)
+					.onTapGesture {
+						showAddToListSheet = false
+					}
+			}
+			.padding(.top, 16)
+			ScrollView {
+				HStack {
+					Image(systemName: "plus")
+						.font(.body)
+						.frame(width: 58, height: 55)
+						.cornerRadius(6)
+						.background(.buttonBlue.opacity(0.05))
+					Text("ADD TO NEW LIST")
+						.font(.karlRegular(size: 14))
+				}
+				.frame(maxWidth: .infinity, alignment: .leading)
+				HStack {
+					Image(systemName: "star.fill")
+						.font(.body)
+						.foregroundStyle(.buttonBlue)
+						.frame(width: 58, height: 55)
+						.cornerRadius(6)
+						.background(.buttonBlue.opacity(0.05))
+					VStack(alignment: .leading) {
+						Text("ADD TO FAVORITES")
+							.font(.karlRegular(size: 14))
+						Text("23 Items")
+							.font(.footnote)
+					}
+				}
+				.frame(maxWidth: .infinity, alignment: .leading)
+				HStack {
+					Image(systemName: "gear")
+						.font(.body)
+						.foregroundStyle(.buttonBlue)
+						.frame(width: 58, height: 55)
+						.cornerRadius(6)
+						.background(.buttonBlue.opacity(0.05))
+					VStack(alignment: .leading) {
+						Text("MORNING TASKS")
+							.font(.karlRegular(size: 14))
+						Text("10 Items")
+							.font(.footnote)
+					}
+				}
+				.frame(maxWidth: .infinity, alignment: .leading)
+				HStack {
+					Image(systemName: "pencil")
+						.font(.body)
+						.foregroundStyle(.buttonBlue)
+						.frame(width: 58, height: 55)
+						.cornerRadius(6)
+						.background(.buttonBlue.opacity(0.05))
+					VStack(alignment: .leading) {
+						Text("DAILY TASKS")
+							.font(.karlRegular(size: 14))
+						Text("1 Item")
+							.font(.footnote)
+					}
+				}
+				.frame(maxWidth: .infinity, alignment: .leading)
+			}
+			.scrollIndicators(.hidden)
+		}
+		.padding(.horizontal, 8)
+		.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
 	}
 }
 
