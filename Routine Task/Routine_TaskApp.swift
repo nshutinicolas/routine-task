bd27cf410e5d9c9a06b4d862568b6ed4dc6a1bd0
nshutinicolas <nshuti.nicolas@yahoo.com>
2024-12-20 07:02:28 +0200

    Initial Commit

---
+++ Routine Task/Routine_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Routine_TaskApp.swift
+//  Routine Task
+//
+//  Created by Musoni nshuti Nicolas on 20/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Routine_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
